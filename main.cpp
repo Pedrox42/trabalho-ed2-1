4b349f36ec094e297e441432c87c73a80a89cb10
annafrancox <annaleticiafranco31@gmail.com>
2021-11-09 18:50:58 -0300

    mudanca do argumento, testes de diretorio

--- main.cpp
+++ main.cpp
@@ -67,35 +67,78 @@ int main(int argc, char const *argv[]) {
     //Verifica se todos os parametros do programa foram entrados
     if (argc != 2)
     {
-        cout << "Erro! Esperado: ./<program_name> <input_file>" << endl;
+        cout << "Erro! Esperado: ./<program_name> <diretorio> <input_file>" << endl;
         return 1;
     }
 
     string program_name(argv[0]);
-    string input_file_name(argv[1]);
+    string diretorio(argv[1])
+    string input_file_name(argv[2]);
+
+    //abrir diretorio
+    DIR* dp = opendir(diretorio.c_str());
+
+    struct dirent *diread;
+    //vetor p guardar os arquivos do diretorio
+    vector<char *> files;
+
+    if (dp == NULL) 
+    {
+        cout << "Erro! Esperado caminho completo do diretorio";
+        perror("opendir");
+        return errno;
+
+    } else {
+        //loop para adicionar os arquivos no vetor
+        while ((diread = readdir(diretorio)) != nullptr) {
+            files.push_back(diread->d_name);
+        }
+    }
+
+    for (auto file : files) cout << file << "| ";
+    cout << endl;
 
     string instance;
-    if(!(input_file_name.find(".csv") <= input_file_name.size()))
-    {
-        cout << "Erro! Esperado arquivo CSV." << endl;
-        return 1;
+
+    bool file_found = false;
+    //Verifica a substring depois do carater acima
+    for (auto file : files) {
+            //Procura o ultimo caracter especificado
+            string found = file.find_last_of(".")
+
+            if((file.substr(found+1))!="bin")
+            {
+                if((file.substr(found+1))=="csv")
+                {
+                    //off:trata o arquivo csv p programa funcionar
+
+                    //Arquivo csv encontrado
+                    file_found=true;
+                    break;
+                }
+            } else{
+                //Arquivo binario encontrado
+                file_found=true;
+                break;
+            }
+        
     }
 
     //Abrindo arquivo de entrada
     ifstream input_file;
     //input_file.open(argv[1], ios::in);
 
-
     Review* review;
     if(input_file.is_open())
     {
         mainMenu(input_file, review);
-    }else
+    }else{
         cout << "Impossibilitado de abrir o arquivo" << endl;
+    }
 
     //Fechando arquivo de entrada
     input_file.close();
-
+    closedir(dp);
     return 0;
 
     //Review* review = new Review("1234abcd", "Muito legal tiktok", "3.2", "22/10/2021", 30);
